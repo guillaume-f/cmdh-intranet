0287301e18c9457076bd237f54cfddb8074bf2ec
IRISAD\gflament <gflament@actiris.be>
2026-05-08 10:13:14 +0200

    activity form

--- front/src/app/features/activities/activities.routes.ts
+++ front/src/app/features/activities/activities.routes.ts
@@ -7,6 +7,10 @@ export const  activitiesRoutes: Routes = [
     loadComponent: () => import('./activity-list/activity-list.component').then(m => m.ActivityListComponent),
   },
   {
+    path: 'new',
+    loadComponent: () => import('./activity-form/activity-form.component').then(m => m.ActivityFormComponent),
+  },
+  {
     path: ':activityId',
     loadComponent: () => import('./activity-detail/activity-detail.component').then(m => m.ActivityDetailComponent),
   }
---
+++ front/src/app/features/activities/activity-form/activity-form.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-activity-form',
+  imports: [],
+  templateUrl: './activity-form.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ActivityFormComponent { }
